8e98bc2aa7524c3e6e13519c3c17aa930b2f9cd0
Yukitoshi Imaizumi-Zhou <yukitoshi.imaizumizhou@gmail.com>
2024-04-06 02:20:06 +1100

    reverted back as that encountered error

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,7 +1,6 @@
 // Import methods to save and get data from the indexedDB database in './database.js'
 import { getDb, putDb } from "./database";
 import { header } from "./header";
-import { CodeMirror } from "code-mirror-themes";
 
 export default class {
   constructor() {
